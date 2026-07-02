1f05bc96856f9016e8e3396321f3a49818fbca3e
João Gabriel s² <15634616+Densuki@users.noreply.github.com>
2026-07-02 11:52:04 -0300

    Add user data module with personal information

---
+++ scripts/data.js
@@ -0,0 +1,13 @@
+module.exports = {
+    birthday: "2000-03-03",
+
+    github: "Densuki",
+
+    location: "Fortaleza",
+
+    country: "Brasil",
+
+    programmingSince: 2023,
+
+    startedGithub: 2022
+};
